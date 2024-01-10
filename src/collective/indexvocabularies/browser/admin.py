12da7c2ce8d21305fa043cfe93b634aabab2a2ea
Jon Pentland <jon.pentland@pretagov.co.uk>
2024-01-10 15:31:48 +0000

    Add fix persistent utility code from wildcard.fixpersistentutilities. Show utilities in admin view.

--- src/collective/indexvocabularies/browser/admin.py
+++ src/collective/indexvocabularies/browser/admin.py
@@ -1,7 +1,11 @@
 from collective.indexvocabularies.utils import _list_index_vocabularies
 from Products.Five.browser import BrowserView
+from plone import api
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
+from zope.component import getSiteManager
+from zope.schema.interfaces import IVocabularyFactory
+
 
 class AdminView(BrowserView):
 
@@ -15,4 +19,7 @@ class AdminView(BrowserView):
             factory = getUtility(IVocabularyFactory, name=f"collective.indexvocabularies.{vocab}")
             self.vocabulary_values = factory(self.context).by_value.keys()
 
+        portal = api.portal.get()
+        lsm = getSiteManager(portal)
+        self.putils = lsm.getUtilitiesFor(IVocabularyFactory)
         return super(AdminView, self).__call__()
